ebd622ef07a302a1ed4a2d2cea61c78495687047
Marc Alen Cuevas <marcalencc@gmail.com>
2017-08-09 15:21:52 -0400

    - added base class for media items

--- MetacriticAPI/Controllers/AlbumController.cs
+++ MetacriticAPI/Controllers/AlbumController.cs
@@ -9,85 +9,11 @@ using MetacriticScraper.Interfaces;
 
 namespace MetacriticAPI.Controllers
 {
-    public class AlbumController : ApiController
+    public class AlbumController : MediaItemController
     {
-        private readonly IMetacriticService m_metacriticService;
-
-        public AlbumController(IMetacriticService metacriticService)
+        public AlbumController(IMetacriticService metacriticService) : base(metacriticService)
         {
-            m_metacriticService = metacriticService;
-        }
-
-        [HttpGet]
-        public IHttpActionResult Get(string title)
-        {
-            HttpStatusCode statusCode;
-            string id = m_metacriticService.GetId();
-            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/album/{0}", title),
-                out statusCode);
-
-            if (statusCode == HttpStatusCode.OK)
-            {
-                return Json(result);
-            }
-            else
-            {
-                return Content(statusCode, result);
-            }
-        }
-
-        [HttpGet]
-        public IHttpActionResult GetDetails(string title, string details)
-        {
-            HttpStatusCode statusCode;
-            string id = m_metacriticService.GetId();
-            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/album/{0}/{1}",
-                title, details), out statusCode);
-
-            if (statusCode == HttpStatusCode.OK)
-            {
-                return Json(result);
-            }
-            else
-            {
-                return Content(statusCode, result);
-            }
-        }
-
-        [HttpGet]
-        public IHttpActionResult GetItemWithYear(string title, int? year)
-        {
-            HttpStatusCode statusCode;
-            string id = m_metacriticService.GetId();
-            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/album/{0}/{1}",
-                title, year), out statusCode);
-
-            if (statusCode == HttpStatusCode.OK)
-            {
-                return Json(result);
-            }
-            else
-            {
-                return Content(statusCode, result);
-            }
-        }
-
-        [HttpGet]
-        public IHttpActionResult Get(string title, int? year, string details)
-        {
-            HttpStatusCode statusCode;
-            string id = m_metacriticService.GetId();
-            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/album/{0}/{1}/{2}",
-                title, year, details), out statusCode);
-
-            if (statusCode == HttpStatusCode.OK)
-            {
-                return Json(result);
-            }
-            else
-            {
-                return Content(statusCode, result);
-            }
+            m_keyword = "album";
         }
     }
 }
---
+++ MetacriticAPI/Controllers/MediaItemController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MetacriticAPI.Services;
+using MetacriticScraper.Interfaces;
+
+namespace MetacriticAPI.Controllers
+{
+    public class MediaItemController : ApiController
+    {
+        protected readonly IMetacriticService m_metacriticService;
+        protected string m_keyword;
+
+        public MediaItemController(IMetacriticService metacriticService)
+        {
+            m_metacriticService = metacriticService;
+        }
+
+        private IHttpActionResult GetReturn(HttpStatusCode statusCode, IMetacriticData[] result)
+        {
+            if (statusCode == HttpStatusCode.OK)
+            {
+                return Json(result);
+            }
+            else
+            {
+                return Content(statusCode, result);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(string title)
+        {
+            HttpStatusCode statusCode;
+            string id = m_metacriticService.GetId();
+            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/{0}/{1}", m_keyword,
+                title), out statusCode);
+
+            return GetReturn(statusCode, result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetDetails(string title, string details)
+        {
+            HttpStatusCode statusCode;
+            string id = m_metacriticService.GetId();
+            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/{0}/{1}/{2}",
+                m_keyword, title, details), out statusCode);
+
+            return GetReturn(statusCode, result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetItemWithYear(string title, int? year)
+        {
+            HttpStatusCode statusCode;
+            string id = m_metacriticService.GetId();
+            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/{0}/{1}/{2}",
+                m_keyword, title, year), out statusCode);
+
+            return GetReturn(statusCode, result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(string title, int? year, string details)
+        {
+            HttpStatusCode statusCode;
+            string id = m_metacriticService.GetId();
+            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/{0}/{1}/{2}/{3}",
+                m_keyword, title, year, details), out statusCode);
+
+            return GetReturn(statusCode, result);
+        }
+    }
+}
