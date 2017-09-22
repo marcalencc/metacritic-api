a25c1e21208ece28d5321f4606cc6b6819acf51b
Marc Alen Cuevas <marcalencc@gmail.com>
2017-09-22 08:58:49 +0800

    - Fixes for search functionality

---
+++ MetacriticAPI/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MetacriticAPI.Services;
+using MetacriticScraper.Interfaces;
+using MetacriticAPI.Helpers;
+
+namespace MetacriticAPI.Controllers
+{
+    public class SearchController : ApiController
+    {
+        protected readonly IMetacriticService m_metacriticService;
+
+        public SearchController(IMetacriticService metacriticService)
+        {
+            m_metacriticService = metacriticService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetDetails(string title, string details)
+        {
+            HttpStatusCode statusCode;
+            string id = m_metacriticService.GetId();
+            string ip = HttpExtensions.GetClientIP(Request);
+            IMetacriticData[] result = m_metacriticService.GetResult(id, string.Format("/search/{0}/{1}",
+                title, details), ip, out statusCode);
+
+            return Content(statusCode, result);
+        }
+    }
+}
